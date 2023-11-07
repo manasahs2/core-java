3d9a45bbf3cf3f23190b6b32b610f93406f198cb
manasahs2 <hsmanasa3@gmail.com>
2023-11-07 11:55:54 +0530

    enum

---
+++ Game.java
@@ -0,0 +1,30 @@
+class Game
+{
+int id;
+String name;
+Players total=Players.VINU;
+
+void play()
+{
+System.out.println("game is hockey");
+}
+
+
+void pause()
+{
+System.out.println("game is pause");
+}
+
+void end()
+{
+System.out.println("game is end");
+}
+
+void showinfo()
+{
+System.out.println("id :"+id);
+System.out.println("name :"+name);
+System.out.println("total :"+total);
+}
+}
+                                                                                                                   
