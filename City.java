ba19848e228a1fdfd19479ec758b38ae915306dd
manasahs2 <hsmanasa3@gmail.com>
2023-11-10 10:24:16 +0530

    array association

---
+++ City.java
@@ -0,0 +1,21 @@
+class City
+{
+String name;
+String StateName;
+
+void setname(String name)
+{
+this.name=name;
+}
+
+void StateName(String StateName)
+{
+this.StateName=StateName;
+}
+
+void showinfo()
+{
+    System.out.println("name is :"+name);
+	System.out.println("StateName is :"+StateName);	
+}
+}
